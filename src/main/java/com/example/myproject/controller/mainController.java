948c6edf5684a634c0a6ae4bbd874e675e799070
Awen <564013650@qq.com>
2019-05-31 18:51:05 +0800

    up

--- src/main/java/com/example/myproject/controller/mainController.java
+++ src/main/java/com/example/myproject/controller/mainController.java
@@ -1,5 +1,7 @@
 package com.example.myproject.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -10,13 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Component
 @EnableScheduling
 public class mainController {
-
+    private static Logger log = LoggerFactory.getLogger(mainController.class);
     int test = 0;
 
     @Transactional
     @Scheduled(cron = "0/3 * * * * ?")
     public void requestMethodName() {
+        log.info("————定时任务开始执行————");
         System.out.println(test++);
+        log.info("————定时任务执行成功————");
         return;
     }
 
